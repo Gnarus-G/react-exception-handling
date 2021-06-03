2726e4ee1b3eb5dac7ee3bbdd11df69065b7f4e4
gnarus-g <g.avet.gnaritas@gmail.com>
2021-06-03 13:40:36 -0400

    trying to to allow auto-imports by clients

---
+++ index.ts
@@ -0,0 +1,10 @@
+import WithFallBack from "./component/WithFallBack";
+import ErrorBoundary from "./component/ErrorBoundary";
+import useProxyState from "./hook/useProxyState";
+
+
+export {
+    WithFallBack,
+    ErrorBoundary,
+    useProxyState
+}
